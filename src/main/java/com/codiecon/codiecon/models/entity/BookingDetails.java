f47e6b8253233ee762da00ee3651af8512d8880f
Chalana D A <chalana.da@gdn-commerce.com>
2018-12-19 16:33:16 +0530

    login entities added

--- src/main/java/com/codiecon/codiecon/models/entity/BookingDetails.java
+++ src/main/java/com/codiecon/codiecon/models/entity/BookingDetails.java
@@ -1,3 +1,99 @@
 package com.codiecon.codiecon.models.entity;
 
-public class BookingDetails {}
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = BookingDetails.TABLE_NAME, uniqueConstraints = @UniqueConstraint(columnNames = {
+    BookingDetails.COLUMN_DATE ,BookingDetails.COLUMN_VEHICLE_ID))
+public class BookingDetails {
+
+  public static final String TABLE_NAME = "booking";
+  public static final String COLUMN_ID = "ID";
+  public static final String COLUMN_DRIVER_ID = "driver_id";
+  public static final String COLUMN_VEHICLE_ID = "vehicle_id";
+  public static final String COLUMN_DATE = "booking_date";
+
+  @Id
+  @GeneratedValue(generator = "uuid")
+  @GenericGenerator(name = "uuid", strategy = "uuid")
+  @Column(name = BookingDetails.COLUMN_ID, nullable = false)
+  private String id;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = BookingDetails.COLUMN_DRIVER_ID)
+  private DriverDetails driverDetails;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = BookingDetails.COLUMN_VEHICLE_ID)
+  private VehicleDetails vehicleDetails;
+
+  @Column(name = BookingDetails.COLUMN_DATE, nullable = false)
+  @DateTimeFormat(pattern = "dd.MM.yyyy")
+  private Date date;
+
+  public BookingDetails(String id, DriverDetails driverDetails, VehicleDetails vehicleDetails, Date date) {
+    this.id = id;
+    this.driverDetails = driverDetails;
+    this.vehicleDetails = vehicleDetails;
+    this.date = date;
+  }
+
+  public BookingDetails() {
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public DriverDetails getDriverDetails() {
+    return driverDetails;
+  }
+
+  public void setDriverDetails(DriverDetails driverDetails) {
+    this.driverDetails = driverDetails;
+  }
+
+  public VehicleDetails getVehicleDetails() {
+    return vehicleDetails;
+  }
+
+  public void setVehicleDetails(VehicleDetails vehicleDetails) {
+    this.vehicleDetails = vehicleDetails;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder("BookingDetails{");
+    sb.append("id='").append(id).append('\'');
+    sb.append(", driverDetails=").append(driverDetails);
+    sb.append(", vehicleDetails=").append(vehicleDetails);
+    sb.append(", date=").append(date);
+    sb.append('}');
+    return sb.toString();
+  }
+}
--- src/main/java/com/codiecon/codiecon/models/entity/DriverDetails.java
+++ src/main/java/com/codiecon/codiecon/models/entity/DriverDetails.java
@@ -1,3 +1,102 @@
 package com.codiecon.codiecon.models.entity;
 
-public class DriverDetails {}
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = DriverDetails.TABLE_NAME)
+public class DriverDetails {
+
+  public static final String TABLE_NAME = "driver";
+  public static final String COLUMN_ID = "ID";
+  public static final String COLUMN_NAME = "name";
+  public static final String COLUMN_DL_NUMBER = "dl_number";
+  public static final String COLUMN_CONTACT_NUMBER = "contact_number";
+  public static final String COLUMN_IS_APPROVED = "is_approved";
+
+  @Id
+  @GeneratedValue(generator = "uuid")
+  @GenericGenerator(name = "uuid", strategy = "uuid")
+  @Column(name = DriverDetails.COLUMN_ID, nullable = false)
+  private String id;
+
+  @Column(name = DriverDetails.COLUMN_NAME, nullable = false)
+  private String name;
+
+  @Column(name = DriverDetails.COLUMN_DL_NUMBER, nullable = false)
+  private String dlNumber;
+
+  @Column(name = DriverDetails.COLUMN_CONTACT_NUMBER, nullable = false)
+  private String contactNumber;
+
+  @Column(name = DriverDetails.COLUMN_IS_APPROVED, nullable = false)
+  private boolean approved;
+
+  public DriverDetails(String id, String name, String dlNumber, String contactNumber, boolean approved) {
+    this.id = id;
+    this.name = name;
+    this.dlNumber = dlNumber;
+    this.contactNumber = contactNumber;
+    this.approved = approved;
+  }
+
+  public DriverDetails() {
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDlNumber() {
+    return dlNumber;
+  }
+
+  public void setDlNumber(String dlNumber) {
+    this.dlNumber = dlNumber;
+  }
+
+  public String getContactNumber() {
+    return contactNumber;
+  }
+
+  public void setContactNumber(String contactNumber) {
+    this.contactNumber = contactNumber;
+  }
+
+  public boolean isApproved() {
+    return approved;
+  }
+
+  public void setApproved(boolean approved) {
+    this.approved = approved;
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder("DriverDetails{");
+    sb.append("id='").append(id).append('\'');
+    sb.append(", name='").append(name).append('\'');
+    sb.append(", dlNumber='").append(dlNumber).append('\'');
+    sb.append(", contactNumber='").append(contactNumber).append('\'');
+    sb.append(", approved=").append(approved);
+    sb.append('}');
+    return sb.toString();
+  }
+}
