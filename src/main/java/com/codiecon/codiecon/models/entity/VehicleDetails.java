bae692032661d46fd6c66505ce12d3a5ca40ee7e
Atul  Jain <atuljain@Atuls-MacBook-Pro.local>
2018-12-19 15:16:21 +0530

    something idk

--- src/main/java/com/codiecon/codiecon/models/entity/VehicleDetails.java
+++ src/main/java/com/codiecon/codiecon/models/entity/VehicleDetails.java
@@ -1,3 +1,9 @@
 package com.codiecon.codiecon.models.entity;
 
+<<<<<<< HEAD
+=======
+import javax.persistence.Entity;
+
+@Entity
+>>>>>>> 59ade33956f4abd06b393cbd96d64d6557b4e193
 public class VehicleDetails {}
