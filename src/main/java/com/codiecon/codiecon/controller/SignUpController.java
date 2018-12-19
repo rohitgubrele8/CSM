1bf27dc6d4e18997383f4fe1230fc8973c7fc36a
Rohit Gubrele <rohitgubrele8@gmail.com>
2018-12-19 19:21:45 +0530

    added BaseResponse

--- src/main/java/com/codiecon/codiecon/controller/SignUpController.java
+++ src/main/java/com/codiecon/codiecon/controller/SignUpController.java
@@ -1,3 +1,15 @@
 package com.codiecon.codiecon.controller;
 
-public class SignUpController {}
+
+import io.swagger.annotations.Api;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "SignUpController", description = "SignUpFlow")
+@RestController
+public class SignUpController {
+
+
+}
