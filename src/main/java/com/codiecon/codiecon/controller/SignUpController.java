aa64b0c82778535d12b1521b3a7dcbbe342d17d3
Atul  Jain <atuljain@Atuls-MacBook-Pro.local>
2018-12-19 19:23:59 +0530

    bank details request copy

--- src/main/java/com/codiecon/codiecon/controller/SignUpController.java
+++ src/main/java/com/codiecon/codiecon/controller/SignUpController.java
@@ -1,7 +1,23 @@
 package com.codiecon.codiecon.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 public class SignUpController {
 
 
+
+  @RequestMapping(value = "vehicleOwner", method = RequestMethod.POST, consumes = MediaType
+      .APPLICATION_JSON_VALUE , produces = MediaType.APPLICATION_JSON_VALUE)
+  public BaseResponse saveBankDetails(
+      @Validated @RequestBody BankDetailsRequest bankDetailsRequest) {
+    LOGGER.debug("Saving Bank Details with values: {}", bankDetailsRequest.toString());
+    userDetailsService.saveBankDetails(bankDetailsRequest);
+    return new BaseResponse(true, HttpStatus.OK.value());
+  }
 
 }
