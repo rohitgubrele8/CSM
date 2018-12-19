3d7118493139286976589eeebcbe6912447e93c2
Rohit Gubrele <rohitgubrele8@gmail.com>
2018-12-19 19:12:38 +0530

    added service and swagger

--- src/main/java/com/codiecon/codiecon/service/SignUpService.java
+++ src/main/java/com/codiecon/codiecon/service/SignUpService.java
@@ -1,3 +1,18 @@
 package com.codiecon.codiecon.service;
 
-public class SignUpService {}
+public interface SignUpService {
+
+  public void driverSignUp();
+
+  public void vehicleOwnerSignUp();
+
+  public void bookVehicle();
+
+  public void updateVehicleAvailability();
+
+  public void cancelBooking();
+
+  public void uploadVehicleDocuments();
+
+  public void uploadDriverDocuments();
+}
